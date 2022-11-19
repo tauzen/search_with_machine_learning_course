48971ce0da781380f03d09db6b88106879ae53f4
Krzysztof Mioduszewski <kmioduszewski@gmail.com>
2022-11-19 22:34:30 +0000

    Perform vector search

--- utilities/query.py
+++ utilities/query.py
@@ -12,6 +12,10 @@ import pandas as pd
 import fileinput
 import logging
 
+import sys
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -186,11 +190,31 @@ def create_query(user_query, click_prior_query, filters, sort="_score", sortDir=
     return query_obj
 
 
-def search(client, user_query, index="bbuy_products", sort="_score", sortDir="desc"):
+def create_vector_query(user_query, size=10):
+    vector = model.encode(user_query)
+    return  {
+        "size": size,
+        "query": {
+            "knn": {
+                "embedding": {
+                    "vector": vector,
+                    "k": size
+                }
+            }
+        },
+        "_source": ["name", "sku", "categoryPath"]
+    }
+
+
+def search(client, user_query, index="bbuy_products", sort="_score", sortDir="desc", vector=False):
     #### W3: classify the query
     #### W3: create filters and boosts
     # Note: you may also want to modify the `create_query` method above
-    query_obj = create_query(user_query, click_prior_query=None, filters=None, sort=sort, sortDir=sortDir, source=["name", "shortDescription"])
+    if not vector:
+        query_obj = create_query(user_query, click_prior_query=None, filters=None, sort=sort, sortDir=sortDir, source=["name", "shortDescription"])
+    else:
+        query_obj = create_vector_query(user_query)
+
     logging.info(query_obj)
     response = client.search(query_obj, index=index)
     if response and response['hits']['hits'] and len(response['hits']['hits']) > 0:
@@ -212,6 +236,7 @@ if __name__ == "__main__":
                          help='The OpenSearch port')
     general.add_argument('--user',
                          help='The OpenSearch admin.  If this is set, the program will prompt for password too. If not set, use default of admin/admin')
+    general.add_argument('--vector', action='store_true', help='Performs semantic search')
 
     args = parser.parse_args()
 
@@ -241,11 +266,11 @@ if __name__ == "__main__":
     index_name = args.index
     query_prompt = "\nEnter your query (type 'Exit' to exit or hit ctrl-c):"
     print(query_prompt)
-    for line in fileinput.input():
+    for line in sys.stdin:
         query = line.rstrip()
         if query == "Exit":
             break
-        search(client=opensearch, user_query=query, index=index_name)
+        search(client=opensearch, user_query=query, index=index_name, vector=args.vector)
 
         print(query_prompt)
 
